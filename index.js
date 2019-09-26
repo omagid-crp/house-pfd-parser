a1ed49b6dcb21579a443ca5347bc9b81f672b3ae
Oren Magid <omagid@crp.org>
2019-09-26 10:08:15 -0400

    handles unreadable pdfs

--- index.js
+++ index.js
@@ -32,111 +32,128 @@ function processFiling(pdfPath, outPath) {
 				doc.getPage(pageNum).then(page => {
 					const viewport = page.getViewport(1.0 /* scale */)
 
-					return page.getTextContent().then(content => {
-						let itemsGroupedByYPos = _.groupBy(content.items, item => item.transform[5])
-						let rowY = 0
-
-						let yPositions = Object.keys(itemsGroupedByYPos)
-							.sort((a, b) => {
-								return a - b
-							})
-							.reverse()
-
-						for (let i = 0; i < yPositions.length; i++) {
-							let yPos = yPositions[i]
-
-							let items = itemsGroupedByYPos[yPos].sort((a, b) => {
-								return a.transform[4] - b.transform[4]
-							})
-
-							let string = ''
-							items.forEach(item => (string += item.str))
-							string = string.toLowerCase()
-
-							if (!ignoreRest) {
-								if (string == 'exclusions of spouse, dependent, or trust information') {
+					return page.getTextContent().then(
+						content => {
+							if (content.items.length === 0) {
+								if (!ignoreRest) {
+									const noContentCSV = `${outPath}no-content.csv`
+									fs.appendFileSync(noContentCSV, `${pdfPath}\n`)
 									ignoreRest = true
-									break
 								}
+							} else {
+								let itemsGroupedByYPos = _.groupBy(content.items, item => item.transform[5])
+								let rowY = 0
 
-								if (string.search(/schedule \w:/) >= 0) {
-									if (tables.length > 0 && tables[tables.length - 1].part == 'schedule i') {
-										ignoreRest = true
-										break
-									}
-
-									let part = string.split(': ')[0]
-									let name = string.split(': ')[1] ? string.split(': ')[1] : part
-
-									tables.push({
-										part,
-										name,
-										cols: [{ name: 'page', slug: 'page' }],
-										rows: [],
+								let yPositions = Object.keys(itemsGroupedByYPos)
+									.sort((a, b) => {
+										return a - b
 									})
-								} else {
-									const currTable = tables[tables.length - 1]
+									.reverse()
 
-									items.forEach(item => {
-										if (currTable) {
-											if (item.transform[0] == 9.75) {
-												let append = true
+								for (let i = 0; i < yPositions.length; i++) {
+									let yPos = yPositions[i]
 
-												currTable.cols.forEach(col => {
-													if (col.x == item.transform[4]) {
-														if (col.name.indexOf(item.str) < 0) {
-															col.name += ` ${item.str}`
-															col.slug = col.name.toLowerCase().replace(/[ ,]+/g, '-')
-														}
-														append = false
-													}
-												})
+									let items = itemsGroupedByYPos[yPos].sort((a, b) => {
+										return a.transform[4] - b.transform[4]
+									})
 
-												if (append) {
-													if (
-														!currTable.cols.find(
-															e => e.name == item.str && e.x === item.transform[4]
-														)
-													) {
-														currTable.cols.push({
-															name: item.str,
-															slug: item.str.toLowerCase().replace(/[ ,]+/g, '-'),
-															x: item.transform[4],
-															y: item.transform[5],
-														})
-													}
-												}
-											} else {
-												if (item.transform[0] >= 9) {
-													currTable.cols.forEach((col, i2) => {
-														if (col.x == item.transform[4]) {
-															//if(item.str.charAt(item.str.length - 1) == '-'){
-															if (i2 > 0 && Math.abs(item.transform[5] - rowY) > 12) {
-																currTable.rows.push({})
-															}
+									let string = ''
+									items.forEach(item => (string += item.str))
+									string = string.toLowerCase()
 
-															const curRow = currTable.rows[currTable.rows.length - 1]
-
-															curRow['page'] = pageNum
-
-															if (col.slug in curRow) {
-																curRow[col.slug] += ' '
-																curRow[col.slug] += item.str
-															} else {
-																curRow[col.slug] = item.str
-															}
-
-															rowY = item.transform[5]
-														}
-													})
-												}
-											}
+									if (!ignoreRest) {
+										if (string == 'exclusions of spouse, dependent, or trust information') {
+											ignoreRest = true
+											break
 										}
-									})
+
+										if (string.search(/schedule \w:/) >= 0) {
+											if (tables.length > 0 && tables[tables.length - 1].part == 'schedule i') {
+												ignoreRest = true
+												break
+											}
+
+											let part = string.split(': ')[0]
+											let name = string.split(': ')[1] ? string.split(': ')[1] : part
+
+											tables.push({
+												part,
+												name,
+												cols: [{ name: 'page', slug: 'page' }],
+												rows: [],
+											})
+										} else {
+											const currTable = tables[tables.length - 1]
+
+											items.forEach(item => {
+												if (currTable) {
+													if (item.transform[0] == 9.75) {
+														let append = true
+
+														currTable.cols.forEach(col => {
+															if (col.x == item.transform[4]) {
+																if (col.name.indexOf(item.str) < 0) {
+																	col.name += ` ${item.str}`
+																	col.slug = col.name
+																		.toLowerCase()
+																		.replace(/[ ,]+/g, '-')
+																}
+																append = false
+															}
+														})
+
+														if (append) {
+															if (
+																!currTable.cols.find(
+																	e => e.name == item.str && e.x === item.transform[4]
+																)
+															) {
+																currTable.cols.push({
+																	name: item.str,
+																	slug: item.str.toLowerCase().replace(/[ ,]+/g, '-'),
+																	x: item.transform[4],
+																	y: item.transform[5],
+																})
+															}
+														}
+													} else {
+														if (item.transform[0] >= 9) {
+															currTable.cols.forEach((col, i2) => {
+																if (col.x == item.transform[4]) {
+																	//if(item.str.charAt(item.str.length - 1) == '-'){
+																	if (
+																		i2 > 0 &&
+																		Math.abs(item.transform[5] - rowY) > 12
+																	) {
+																		currTable.rows.push({})
+																	}
+
+																	const curRow =
+																		currTable.rows[currTable.rows.length - 1]
+
+																	curRow['page'] = pageNum
+
+																	if (col.slug in curRow) {
+																		curRow[col.slug] += ' '
+																		curRow[col.slug] += item.str
+																	} else {
+																		curRow[col.slug] = item.str
+																	}
+
+																	rowY = item.transform[5]
+																}
+															})
+														}
+													}
+												}
+											})
+										}
+									}
 								}
 							}
-						}
-					})
+						},
+						error => console.log(error)
+					)
 				})
 
 			lastPromise = loadPage(1)
@@ -218,6 +235,9 @@ function processFiling(pdfPath, outPath) {
 }
 
 const run = (filePath, outPath) => {
+	const noContentCSV = `${outPath}no-content.csv`
+	fs.appendFileSync(noContentCSV, `File\n`)
+
 	const files = fs.readdirSync(filePath).filter(file => file.toLowerCase().includes('.pdf'))
 
 	let filingPromise = processFiling(filePath + files[0], outPath)
@@ -230,5 +250,6 @@ const run = (filePath, outPath) => {
 }
 
 ;(() => {
-	run(process.argv[3] || `${__dirname}/data/input/`, process.argv[4] || `${__dirname}/data/output/`)
+	process.argv
+	run(process.argv[2] || `${__dirname}/data/input/`, process.argv[3] || `${__dirname}/data/output/`)
 })()
